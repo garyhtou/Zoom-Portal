351880bd33202d24421ce731435b1cfdde9262f4
Gary Tou <gary@garytou.com>
2021-06-11 18:15:57 -0700

    hook up join button

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,11 @@ import {
 	Spinner,
 	Grid,
 	GridItem,
+	Link,
 } from '@chakra-ui/react';
 import One from '../components/one';
 import moment from 'moment';
+import isMobile from 'is-mobile';
 
 export default function Home() {
 	const [calls, setCalls] = useState([]);
@@ -140,24 +142,27 @@ export function Call(props) {
 							>
 								Message
 							</Button> */}
-					<Button
-						flex={1}
-						fontSize={'sm'}
-						rounded={'full'}
-						bg={'blue.400'}
-						color={'white'}
-						boxShadow={
-							'0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-						}
-						_hover={{
-							bg: 'blue.500',
-						}}
-						_focus={{
-							bg: 'blue.500',
-						}}
-					>
-						Join
-					</Button>
+					<Link href={c.join[isMobile() ? 'mobile' : 'desktop']} width={'full'}>
+						<Button
+							flex={1}
+							fontSize={'sm'}
+							rounded={'full'}
+							bg={'blue.400'}
+							color={'white'}
+							boxShadow={
+								'0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+							}
+							_hover={{
+								bg: 'blue.500',
+							}}
+							_focus={{
+								bg: 'blue.500',
+							}}
+							width={'full'}
+						>
+							Join
+						</Button>
+					</Link>
 				</Stack>
 			</Box>
 		</GridItem>
--- utils/socketio.js
+++ utils/socketio.js
@@ -1,7 +1,10 @@
 import { io } from 'socket.io-client';
 
+const FORCE_PROD = false;
 const GARY_TOU_API_DOMAIN =
-	process.env.NODE_ENV === 'development' ? 'localhost:4000' : 'api.garytou.com';
+	!FORCE_PROD && process.env.NODE_ENV === 'development'
+		? 'localhost:4000'
+		: 'api.garytou.com';
 const SOCKET_IO_NAMESPACE = '/slash-z';
 
 export default io(GARY_TOU_API_DOMAIN + SOCKET_IO_NAMESPACE);
